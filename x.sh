c9d7b845970b2cc703e6d4433754377333b78c8a
Neil <neil@Neils-MacBook-Pro.local>
2015-06-02 15:14:27 -0700

    Writing out to txt file and then catting up a xml file

--- x.sh
+++ x.sh
@@ -1,3 +1,5 @@
-(browserName='internet explorer' version=10 platform='Windows 7' ./node_modules/.bin/parallel-cucumber-js features -f json -w 8 | ./node_modules/.bin/cucumber-junit > output_JUnit_ie10.xml) &
-(browserName=chrome version=43 platform='Windows 7' ./node_modules/.bin/parallel-cucumber-js features -f json -w 8 | ./node_modules/.bin/cucumber-junit > output_JUnit_chrome.xml) &
-browserName=firefox version=37 platform='Windows 7' ./node_modules/.bin/parallel-cucumber-js features -f json -w 8 | ./node_modules/.bin/cucumber-junit > output_JUnit_firefox.xml
+(browserName='internet explorer' version=10 platform='Windows 7' ./node_modules/.bin/parallel-cucumber-js features -f json -w 8 > output_JUnit_ie10.txt) &
+(browserName=chrome version=43 platform='Windows 7' ./node_modules/.bin/parallel-cucumber-js features -f json -w 8 > output_JUnit_chrome.txt) &
+browserName=firefox version=37 platform='Windows 7' ./node_modules/.bin/parallel-cucumber-js features -f json -w 8 > output_JUnit_firefox.txt
+
+cat *.txt | ./node_modules/.bin/cucumber-junit > output_JUnit.xml
