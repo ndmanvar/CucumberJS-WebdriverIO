3ca96b9c4e82f03b2397d9b1f6fbe1729b9ad662
Neil <neil@Neils-MacBook-Pro.local>
2015-06-02 15:20:43 -0700

    Using seperate cats

--- x.sh
+++ x.sh
@@ -2,4 +2,6 @@
 (browserName=chrome version=43 platform='Windows 7' ./node_modules/.bin/parallel-cucumber-js features -f json -w 8 > output_JUnit_chrome.txt) &
 browserName=firefox version=37 platform='Windows 7' ./node_modules/.bin/parallel-cucumber-js features -f json -w 8 > output_JUnit_firefox.txt
 
-cat *.txt | ./node_modules/.bin/cucumber-junit > output_JUnit.xml
+cat output_JUnit_firefox.txt | ./node_modules/.bin/cucumber-junit > output_JUnit_firefox.xml
+cat output_JUnit_chrome.txt | ./node_modules/.bin/cucumber-junit > output_JUnit_chrome.xml
+cat output_JUnit_ie10.txt | ./node_modules/.bin/cucumber-junit > output_JUnit_ie10.xml
