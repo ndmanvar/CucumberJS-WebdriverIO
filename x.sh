95cadb146780b5f640856705955f56b23470ceee
Neil <neil@Neils-MacBook-Pro.local>
2015-06-02 14:42:00 -0700

    Specifying -w 8

--- x.sh
+++ x.sh
@@ -1,2 +1,3 @@
-(browserName=chrome version=43 platform='Windows 7' ./node_modules/.bin/parallel-cucumber-js features -f json | ./node_modules/.bin/cucumber-junit > output_JUnit_chrome.xml) &
-browserName=firefox version=37 platform='Windows 7' ./node_modules/.bin/parallel-cucumber-js features -f json | ./node_modules/.bin/cucumber-junit > output_JUnit_firefox.xml
+(browserName=chrome version=43 platform='Windows 7' ./node_modules/.bin/parallel-cucumber-js features -f json -w 8 | ./node_modules/.bin/cucumber-junit > output_JUnit_chrome.xml) &
+browserName=firefox version=37 platform='Windows 7'
+ ./node_modules/.bin/parallel-cucumber-js features -f json -w 8 | ./node_modules/.bin/cucumber-junit > output_JUnit_firefox.xml
