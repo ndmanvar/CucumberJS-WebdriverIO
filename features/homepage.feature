88d67e3fc8080c241d89c16d6d12f405524ceaee
Neil <neil@Neils-MacBook-Pro.local>
2015-06-02 11:50:13 -0700

    updating with nfl draft city test cases

---
+++ features/homepage.feature
@@ -0,0 +1,9 @@
+Feature: Google for Cucumber
+  In order to get more info about Cucumber
+  As a BDDer
+  I want to find Cucumber resources on Google
+
+  Scenario: Verify Draft City Homepage UI
+    Given I am on the draft city chicago homepage
+    Then I should see the draft city chicago logo image
+    Then I should see the draft city chicago homepage text
--- features/step_definitions/example.js
+++ features/step_definitions/example.js
@@ -1,21 +1,93 @@
 module.exports = function () {
 
-  this.Given("I am on Google", function(callback) {
+  this.Given("I am on the draft city chicago homepage", function(callback) {
     this.client
-      .url('http://google.com')
+      .url('http://www.nfl.com/draft-city-chicago')
       .call(callback);
   });
 
-  this.When('I search for "$query"', function(query, callback) {
+  this.Then('I should see the draft city chicago logo image', function(callback) {
     this.client
-      .setValue('*[name="q"]', query)
+      .waitForVisible('.index-content img', 3000).should.eventually.be.true
       .call(callback);
   });
 
-  this.Then('I see a link to "$url"', function(url, callback) {
+  this.Then('I should see the draft city chicago homepage text', function(callback) {
     this.client
-      .waitForExist('a[href*="' + url + '"]', 3000)
-      .should.eventually.be.true
+      .waitForText('.index-content div', 3000).should.eventually.be.true
+      .getText('.index-content div').should.eventually.contain("For the first time in 51 years,")
+      .call(callback);
+  });
+
+  this.When('I click the explore icon', function (callback) {
+    var selector = '.index-content span';
+
+    this.client
+      .waitForVisible(selector, 3000).should.eventually.be.true
+      .click(selector)
+      .call(callback);
+  });
+
+  this.Then('I should see the Auditorium Theatre header and text', function(callback) {
+    var selector = '.header-left';
+
+    this.client
+      .waitForVisible(selector, 3000).should.eventually.be.true
+      .getText(selector).should.eventually.contain("AUDITORIUM THEATRE")
+      .call(callback);
+  });
+
+  this.Then('I should see a profile picture for "$player"', function(player, callback) {
+    var selector = 'div.content div div div img';
+
+    this.client
+      .waitForVisible(selector, 3000).should.eventually.be.true
+      .getAttribute(selector, 'src').should.eventually.contain(player.replace(' ', '-'))
+      .call(callback);
+  });
+
+  this.Then('I should see a bio entry for "$player"', function(player, callback) {
+    var selector = 'div.content';
+
+    this.client
+      .waitForVisible(selector, 3000).should.eventually.be.true
+      .getText(selector).should.eventually.contain(player.toUpperCase())
+      .call(callback);
+  });
+
+  this.When('I click on "$player" from the player menu toolbar', function(player, callback) {
+    var selector = '[data-person-name="' + player + '"]';
+
+    this.client
+      .waitForVisible(selector, 3000).should.eventually.be.true
+      .click(selector)
+      .call(callback);
+  });
+
+  this.When('I click on the map icon in the right rail', function (callback) {
+    var selector = "div[style*='icn-map.svg']";
+
+    this.client
+      .waitForVisible(selector, 3000).should.eventually.be.true
+      .click(selector)
+      .call(callback);
+  });
+
+  this.When('I click on "$location" in the expanded svg map', function (location, callback) {
+    var selector = "#" + location.toLowerCase().replace(/ /gi, '-') + '-clickzone';
+
+    this.client
+      .waitForVisible(selector, 3000).should.eventually.be.true
+      .click(selector)
+      .call(callback);
+  });
+
+  this.Then('I should see the overlay of "$content"', function (content, callback) {
+    var selector = '.header-left';
+
+    this.client
+      .waitForVisible(selector, 3000).should.eventually.be.true
+      .getText(selector).should.eventually.contain(content.toUpperCase())
       .call(callback);
   });
 
