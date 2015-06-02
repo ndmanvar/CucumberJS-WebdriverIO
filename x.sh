774361eae81d56069f9a4b45bc5a7171212ae530
Neil <neil@Neils-MacBook-Pro.local>
2015-06-02 14:36:50 -0700

    Creating junit reports

--- x.sh
+++ x.sh
@@ -1,2 +1,2 @@
-browserName=chrome version=43 platform='Windows 7' ./node_modules/.bin/parallel-cucumber-js features &
-browserName=firefox version=37 platform='Windows 7' ./node_modules/.bin/parallel-cucumber-js features
+(browserName=chrome version=43 platform='Windows 7' ./node_modules/.bin/parallel-cucumber-js features -f json | ./node_modules/.bin/cucumber-junit > output_JUnit_chrome.xml) &
+browserName=firefox version=37 platform='Windows 7' ./node_modules/.bin/parallel-cucumber-js features -f json | ./node_modules/.bin/cucumber-junit > output_JUnit_firefox.xml
