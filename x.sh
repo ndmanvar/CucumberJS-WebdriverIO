1cf9dbcb4998c8777f67c2036f35d8f49d04124a
Neil <neil@Neils-MacBook-Pro.local>
2015-06-02 14:58:36 -0700

    removed newline

--- x.sh
+++ x.sh
@@ -1,4 +1,3 @@
 (browserName='internet explorer' version=10 platform='Windows 7' ./node_modules/.bin/parallel-cucumber-js features -f json -w 8 | ./node_modules/.bin/cucumber-junit > output_JUnit_ie10.xml) &
 (browserName=chrome version=43 platform='Windows 7' ./node_modules/.bin/parallel-cucumber-js features -f json -w 8 | ./node_modules/.bin/cucumber-junit > output_JUnit_chrome.xml) &
-browserName=firefox version=37 platform='Windows 7'
- ./node_modules/.bin/parallel-cucumber-js features -f json -w 8 | ./node_modules/.bin/cucumber-junit > output_JUnit_firefox.xml
+browserName=firefox version=37 platform='Windows 7' ./node_modules/.bin/parallel-cucumber-js features -f json -w 8 | ./node_modules/.bin/cucumber-junit > output_JUnit_firefox.xml
