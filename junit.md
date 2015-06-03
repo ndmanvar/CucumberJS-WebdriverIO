c4f01f84fd9cfd15de24dd424584a89dc1a3dea4
Neil <neil@Neils-MacBook-Pro.local>
2015-06-02 20:08:32 -0700

    removing unused files

--- junit.md
+++
@@ -1,7 +0,0 @@
-cucumber-js --format=json features/ | node_modules/cucumber-junit/bin/cucumber-junit > output_junit.xml
-
-
-browserName=chrome version=31 platform=XP name=something ./node_modules/.bin/parallel-cucumber-js -w 4 features/
-
-
-browserName=chrome version=31 platform=XP name=something ./node_modules/.bin/parallel-cucumber-js -w 4 features/ -f json | ./node_modules/.bin/cucumber-junit > output_JUnit.xml
--- run_tests.sh
+++
@@ -1,7 +0,0 @@
-(browserName='internet explorer' version=10 platform='Windows 7' ./node_modules/.bin/parallel-cucumber-js features -f json -w 8 > output_JUnit_ie10.txt) &
-(browserName=chrome version=43 platform='Windows 7' ./node_modules/.bin/parallel-cucumber-js features -f json -w 8 > output_JUnit_chrome.txt) &
-browserName=firefox version=37 platform='Windows 7' ./node_modules/.bin/parallel-cucumber-js features -f json -w 8 > output_JUnit_firefox.txt
-
-cat output_JUnit_firefox.txt | ./node_modules/.bin/cucumber-junit > output_JUnit_firefox.xml
-cat output_JUnit_chrome.txt | ./node_modules/.bin/cucumber-junit > output_JUnit_chrome.xml
-cat output_JUnit_ie10.txt | ./node_modules/.bin/cucumber-junit > output_JUnit_ie10.xml
